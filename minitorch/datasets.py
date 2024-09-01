9c2b8a4a5918d1b7ab165028c4920344cac4fa1f
sniemann <sascha.niemann66@gmx.de>
2024-09-01 10:09:44 +0200

    after assignment 1

--- minitorch/datasets.py
+++ minitorch/datasets.py
@@ -4,7 +4,18 @@ from dataclasses import dataclass
 from typing import List, Tuple
 
 
-def make_pts(N):
+def make_pts(N: int) -> List:
+    """Create random 2D points
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Iterable: An Iterable of the N points.
+
+    """
     X = []
     for i in range(N):
         x_1 = random.random()
@@ -20,7 +31,18 @@ class Graph:
     y: List[int]
 
 
-def simple(N):
+def simple(N: int) -> Graph:
+    """Create a Graph with samples that can simply be split at x = 0.5.
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -29,7 +51,18 @@ def simple(N):
     return Graph(N, X, y)
 
 
-def diag(N):
+def diag(N: int) -> Graph:
+    """Create a Graph with samples that can simply be split at y = x.
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -38,7 +71,19 @@ def diag(N):
     return Graph(N, X, y)
 
 
-def split(N):
+def split(N: int) -> Graph:
+    """Create a Graph with samples where one class belongs to the area between x = 0.25 and x = 0.75.
+    And the other class belongs to the rest.
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -47,7 +92,18 @@ def split(N):
     return Graph(N, X, y)
 
 
-def xor(N):
+def xor(N: int) -> Graph:
+    """Create a Graph with samples that can be classified like a logical XOR.
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -56,7 +112,18 @@ def xor(N):
     return Graph(N, X, y)
 
 
-def circle(N):
+def circle(N: int) -> Graph:
+    """Create a Graph with samples that can be classified as inside a circle or not.
+
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -66,20 +133,42 @@ def circle(N):
     return Graph(N, X, y)
 
 
-def spiral(N):
+def spiral(N: int) -> Graph:
+    """Create a Graph with samples that form a spiral with two different classes.
 
-    def x(t):
+    Args:
+    ----
+        N (int): The number of points.
+
+    Returns:
+    -------
+        Graph: Graph containing the annotated points.
+
+    """
+
+    def x(t: float) -> float:
         return t * math.cos(t) / 20.0
 
-    def y(t):
+    def y(t: float) -> float:
         return t * math.sin(t) / 20.0
-    X = [(x(10.0 * (float(i) / (N // 2))) + 0.5, y(10.0 * (float(i) / (N //
-        2))) + 0.5) for i in range(5 + 0, 5 + N // 2)]
-    X = X + [(y(-10.0 * (float(i) / (N // 2))) + 0.5, x(-10.0 * (float(i) /
-        (N // 2))) + 0.5) for i in range(5 + 0, 5 + N // 2)]
+
+    X = [
+        (x(10.0 * (float(i) / (N // 2))) + 0.5, y(10.0 * (float(i) / (N // 2))) + 0.5)
+        for i in range(5 + 0, 5 + N // 2)
+    ]
+    X = X + [
+        (y(-10.0 * (float(i) / (N // 2))) + 0.5, x(-10.0 * (float(i) / (N // 2))) + 0.5)
+        for i in range(5 + 0, 5 + N // 2)
+    ]
     y2 = [0] * (N // 2) + [1] * (N // 2)
     return Graph(N, X, y2)
 
 
-datasets = {'Simple': simple, 'Diag': diag, 'Split': split, 'Xor': xor,
-    'Circle': circle, 'Spiral': spiral}
+datasets = {
+    "Simple": simple,
+    "Diag": diag,
+    "Split": split,
+    "Xor": xor,
+    "Circle": circle,
+    "Spiral": spiral,
+}
